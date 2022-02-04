3b99612fe505b78f2e24b2e3e4dafb11e6808334
sbailouni <97708686+sbailouni@users.noreply.github.com>
2022-02-04 02:54:35 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void subtraction() {
-        assertEquals(2, SkillDemo.subtract(3, 2)); //Fails at first
+        assertEquals(1, SkillDemo.subtract(3, 2)); //Fails at first
     }
 }
